5f03878420812581588808d699682f838edb0be3
UltimateTrainer <MitchellNguyen0@gmail.com>
2022-08-08 09:58:40 -0500

    first commit

---
+++ src/test/java/com/ReviewSite/ReviewSite/ReviewSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReviewSite.ReviewSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
