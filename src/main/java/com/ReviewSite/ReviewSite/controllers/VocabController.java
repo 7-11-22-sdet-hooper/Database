8223ae2bf7eaabfe8de401693b53ac3d979b8c4f
dstarr1492 <dstarr1492@gmail.com>
2022-08-09 11:03:06 -0500

    controller added

---
+++ src/main/java/com/ReviewSite/ReviewSite/controllers/VocabController.java
@@ -0,0 +1,21 @@
+package com.ReviewSite.ReviewSite.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ReviewSite.ReviewSite.model.Vocab;
+
+@RestController
+public class VocabController {
+	
+	@GetMapping("/")
+	public Vocab getVocab() {
+		
+		Vocab inheritance = new Vocab("OOP", "inheritance", "One of the four pillars of OOP, this solves the problem of redundant code by allowing more specific versions of a class to use an existing implementation of fields or methods");
+
+		return inheritance;
+	}
+}
+	
+
---
+++ src/main/java/com/ReviewSite/ReviewSite/model/Vocab.java
@@ -0,0 +1,16 @@
+package com.ReviewSite.ReviewSite.model;
+
+public class Vocab {
+public String category;
+public String keyWord;
+public String defition;
+
+public Vocab(String category, String keyWord, String defition) {
+	super();
+	this.category = category;
+	this.keyWord = keyWord;
+	this.defition = defition;
+}
+
+
+}
