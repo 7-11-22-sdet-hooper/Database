8a804187306d445d2a169708e48d793aaa2e9ee8
compscirunner <compscirunner@gmail.com>
2022-08-15 16:03:02 -0400

    Moved and changed webconfig

--- src/main/java/com/ReviewSite/ReviewSite/WebConfig.java
+++ src/main/java/com/ReviewSite/ReviewSite/WebConfig.java
@@ -1,4 +1,4 @@
-package com;
+package com.ReviewSite.ReviewSite;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("GET","POST","PUT","PATCH","DELETE");
     }
 }
