7deede2f1a78ddad121e36d668792ac14361a916
compscirunner <compscirunner@gmail.com>
2022-08-29 15:50:02 -0400

    shortened lorem

--- data.sql
+++ data.sql
@@ -44,7 +44,7 @@ INSERT INTO vocab (`category_id`, `word`, `definition`) VALUES
 (1,"Object",""),
 (1,"Object","✅😊😂🤣❤️😍👌😒😘💕😁"),
 (999,'TEST VOCAB WORD','TEST DEFINITION'),
-(999,'Pneumonoultramicroscopicsilicovolcanoconiosis','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+(999,'Pneumonoultramicroscopicsilicovolcanoconiosis','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,');
 
 
 
