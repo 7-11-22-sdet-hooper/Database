0362c79012877f69df2e93cb578076b8707e55f0
Nathan Shaddock <na.shaddock@gmail.com>
2022-08-29 12:45:47 -0500

    scripts fixed

--- data.sql
+++ data.sql
@@ -1,6 +1,25 @@
-#INSERT INTO `test_schema`.`category` VALUES (999, 'TEST CATEGORY');
+USE test_schema;
 
-INSERT INTO `test_schema`.`multiple_choice`
+# disable foreign key safety (normally prevents TRUNCATE and DELETE or other actions that would break the relationship)
+SET FOREIGN_KEY_CHECKS = 0;
+# drop all old data
+TRUNCATE multiple_choice;
+TRUNCATE vocab;
+TRUNCATE category;
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO category VALUES
+(1, "Java"),
+(2, "asdfasfasfasdf"),
+(3, "Project Manager"),
+(4, "Testing"),
+(5, "Jenkins"),
+(6, "Selenium"),
+(7, "OOP"),
+(8, "Cucumber"),
+(999, "TEST CATEGORY");
+
+INSERT INTO `multiple_choice`
 (`category_id`,
 `question`,
 `a`,
@@ -17,7 +36,7 @@ VALUES
 'TEST ANSWER D',
 'A');
 
-INSERT INTO `test_schema`.`vocab`
+INSERT INTO `vocab`
 (`category_id`,
 `word`,
 `definition`)
--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-USE mysql;
-DROP SCHEMA IF EXISTS test_schema;
-CREATE SCHEMA test_schema;
 USE test_schema;
+DROP TABLE IF EXISTS `test_schema`.`multiple_choice`;
+DROP TABLE IF EXISTS `test_schema`.`vocab`;
+DROP TABLE IF EXISTS `test_schema`.`category`;
 
 CREATE TABLE `category` (
   `id` int UNSIGNED NOT NULL,
@@ -22,19 +22,19 @@ CREATE TABLE `vocab` (
 CREATE TABLE `multiple_choice` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `category_id` int UNSIGNED NOT NULL,
-  `question` varchar(255) NOT NULL,
-  `a` varchar(200),
-  `b` varchar(200),
-  `c` varchar(200),
-  `d` varchar(200),
+  `question` varchar(500) NOT NULL,
+  `a` varchar(500),
+  `b` varchar(500),
+  `c` varchar(500),
+  `d` varchar(500),
   `correct_answer` varchar(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_MultipleChoice_Category_id` (`category_id`),
   CONSTRAINT `FK_MultipleChoice_Category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `login` (
-	`id` int unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `login` (
+        `id` int unsigned NOT NULL AUTO_INCREMENT,
     `username` varchar(50) DEFAULT NULL,
     `password` varchar(255) DEFAULT NULL,
     `password_salt` varchar(50) DEFAULT NULL,
