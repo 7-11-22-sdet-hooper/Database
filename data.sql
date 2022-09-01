8f199c87355bfca56271d780656fb2c5f0f50102
ultimatetrainer <mitchellnguyen0@gmail.com>
2022-09-01 11:01:11 -0500

    Fixed up some organization errors in merge

--- data.sql
+++ data.sql
@@ -23,8 +23,8 @@ INSERT INTO category VALUES # (int <id>, string <name>)
 (6, "Selenium"),
 (7, "OOP"),
 (8, "Cucumber"),
-(15, "REST");
-(9, "SOA");
+(9, "SOA"),
+(10, "REST");
 
 INSERT INTO vocab (`category_id`, `word`, `definition`) VALUES # (int <category_id>, string <word>, string <definition>)
 (7,"inheritance","One of the four pillars of OOP, this solves the problem of redundant code by allowing more specific versions of a class to use an existing implementation of fields/methods."),
@@ -82,9 +82,9 @@ INSERT INTO multiple_choice (`category_id`, `question`, `a`, `b`, `c`, `d`, `cor
 (4,"Test to ensure that the application can handle the expected usage (Max users, max concurrent actions, etc).","Load","Regression","Unit Tests","Accessibility","A"),
 (4,"This test checks to see if all parts of the program are accessible and if they require any specific augmentations for usage by their target audience.","Stress","Performance","Functional","Accessibility","D"),
 (8,"Which of the following is not a Gherkin keyword?","Given","When","For","Then","C"),
-(15,"Which of these are NOT a REST API command?","POST","DELETE", "UPDATE", "GET","C"),
-(15,"What type of request does REST sends?","XML","HTML","JSON","TXT","B"),
-(15,"What is the status code is a success or ok?","200","405","202","369","A");
+(10,"Which of these are NOT a REST API command?","POST","DELETE", "UPDATE", "GET","C"),
+(10,"What type of request does REST sends?","XML","HTML","JSON","TXT","B"),
+(10,"What is the status code is a success or ok?","200","405","202","369","A");
 (4,"Which of the following is not a phase of the SDLC?","Planning","Design","Testing/Integration","Verification","D"),
 (4,"In the SDLC, which word best matches the phase in which software is actually written out?", "Testing", "Design", "Implementation", "Deployment","C");
 (9,"SOAP is used in?", "RESTful API's","Web Services", "Monoliths","Graphic Rendering");
